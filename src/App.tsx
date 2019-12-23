87f8f4f0ab64c1e912150184a657bc17b36ad08d
Daniel Miller (Earthmark) <earthmark.miller@gmail.com>
2019-12-22 20:44:40 -0800

    Added material ui, and started building the referenced sliders.

--- src/App.tsx
+++ src/App.tsx
@@ -11,30 +11,28 @@ const App: React.FC = () => {
   const [geometry1] = useDisposeState(() => new TorusBufferGeometry(10, 1, 16, 100));
   const [geometry2] = useDisposeState(() => new TorusBufferGeometry(5, 3, 16, 100));
 
-  const [materials, setMaterials] = React.useState<(Material | undefined)[]>([undefined, undefined, undefined]);
+  const [materials, setMaterials] = React.useState<(Material | undefined)[]>([undefined]);
 
   return (
-    <div>
-      {
-        materials.map((_, i) => <MaterialPicker key={i} materialUpdated={newMaterial => setMaterials(s => {
-          const newCopy = [...s];
-          newCopy[i] = newMaterial;
-          return newCopy;
-        })} />)
-      }
-      <Renderer height={800} width={800}
-        createCamera={(aspectRatio) => new PerspectiveCamera(75, aspectRatio, 0.1, 1000)}
-        createControls={(camera, canvas) => new OrbitControls(camera, canvas)}>
-        {renderTarget => <>
-          <Mesh material={materials[0]} geometry={geometry1} />
-          <Mesh material={materials[1]} geometry={geometry2} />
-          <RenderObj value={() => new AmbientLight(0x333333)} ></RenderObj>
-          <RenderObj value={() => new DirectionalLight(0xFFFFFF, 1.0)} ></RenderObj>
-          <canvas ref={renderTarget}></canvas>
-        </>
+    <Renderer height={800} width={800}
+      createCamera={(aspectRatio) => new PerspectiveCamera(75, aspectRatio, 0.1, 1000)}
+      createControls={(camera, canvas) => new OrbitControls(camera, canvas)}>
+      {renderTarget => <>
+        <canvas ref={renderTarget}></canvas>
+        <Mesh material={materials[0]} geometry={geometry1} />
+        <Mesh material={materials[0]} geometry={geometry2} />
+        <RenderObj value={() => new AmbientLight(0x333333)} ></RenderObj>
+        <RenderObj value={() => new DirectionalLight(0xFFFFFF, 1.0)} ></RenderObj>
+        {
+          materials.map((_, i) => <MaterialPicker key={i} materialUpdated={newMaterial => setMaterials(s => {
+            const newCopy = [...s];
+            newCopy[i] = newMaterial;
+            return newCopy;
+          })} />)
         }
-      </Renderer>
-    </div>
+      </>
+      }
+    </Renderer>
   );
 }
 
--- src/MaterialPicker.tsx
+++ src/MaterialPicker.tsx
@@ -1,13 +1,14 @@
 import React, { useEffect } from "react";
 import useDisposeState from "./render/useDisposeState";
-import { MeshStandardMaterial, Material } from "three";
+import { MeshPhysicalMaterial, Material } from "three";
+import { Slider, Typography, Paper } from "@material-ui/core";
 
 interface MaterialPickerProps {
   materialUpdated?(material?: Material): void;
 }
 
 const MaterialPicker: React.FC<MaterialPickerProps> = ({ materialUpdated }) => {
-  const [material] = useDisposeState(() => new MeshStandardMaterial());
+  const [material] = useDisposeState(() => new MeshPhysicalMaterial({}));
 
   useEffect(() => {
     materialUpdated?.(material);
@@ -17,15 +18,51 @@ const MaterialPicker: React.FC<MaterialPickerProps> = ({ materialUpdated }) => {
   }, [material]);
 
   // TODO: Picker props
-  return <div>
-    <input value={material.roughness} onChange={e => {
-      const val = parseInt(e.currentTarget.value, 10);
-      if (!isNaN(val)) {
-        material.setValues({ roughness: val });
-      }
-    }}>
-    </input>
-  </div>
+  return <Paper>
+    <Typography>
+      Material Information
+    </Typography>
+    <Typography id="roughness-slider" gutterBottom>
+      Roughness
+    </Typography>
+    <Slider
+      aria-labelledby="roughness-slider"
+      defaultValue={material.roughness}
+      min={0}
+      step={0.05}
+      max={1}
+      onChange={(_, n) => material.roughness = n as number} />
+    <Typography id="metalness-slider" gutterBottom>
+      Metalness
+    </Typography>
+    <Slider
+      aria-labelledby="metalness-slider"
+      defaultValue={material.metalness}
+      min={0}
+      step={0.05}
+      max={1}
+      onChange={(_, n) => material.metalness = n as number} />
+    <Typography id="clearcoat-slider" gutterBottom>
+      Clearcoat
+      </Typography>
+    <Slider
+      aria-labelledby="clearcoat-slider"
+      defaultValue={material.clearcoat}
+      min={0}
+      step={0.05}
+      max={1}
+      onChange={(_, n) => material.clearcoat = n as number} />
+    <Typography id="clearcoat-roughness-slider" gutterBottom>
+      Clearcoat Roughness
+        </Typography>
+    <Slider
+      aria-labelledby="clearcoat-roughness-slider"
+      defaultValue={material.clearcoatRoughness}
+      min={0}
+      step={0.05}
+      max={1}
+      onChange={(_, n) => material.clearcoatRoughness = n as number} />
+  </Paper>
 }
 
 export default MaterialPicker;
